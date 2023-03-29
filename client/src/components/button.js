40ae14c01be676da228285e71cea52d4893d8f95
Can Le <canle360@gmail.com>
2023-03-29 15:19:13 -0600

    add modal to addproduct button

--- client/src/components/button.js
+++ client/src/components/button.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/button.css";
+import ReactModal from "react-modal";
+import { modalStyles } from "../styles/modalStyle";
 const Button = ({ text, onclick }) => {
-  return <button>{text}</button>;
+  const [modalIsOpen, setIsOpen] = useState(false);
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+  const afterOpenModal = () => {};
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div>
+      <button onClick={openModal}>{text}</button>
+      <ReactModal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={modalStyles}
+      >
+        <h2>Add product</h2>
+      </ReactModal>
+    </div>
+  );
 };
 
 export default Button;
