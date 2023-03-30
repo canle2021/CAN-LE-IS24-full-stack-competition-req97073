62edab7dadc62a48b5e09b104f538d6e22455187
Can Le <canle360@gmail.com>
2023-03-29 21:20:13 -0600

    add some information to show when there is not product in data list

--- client/src/components/table.js
+++ client/src/components/table.js
@@ -27,7 +27,7 @@ const Table = () => {
         setProductsData(data.data || []);
       })
       .catch((err) => {
-        console.log("err", err);
+        console.log("fetching data error:", err);
       })
       .finally(() => {
         setLoading(false);
@@ -43,39 +43,46 @@ const Table = () => {
     />
   ) : (
     <div>
-      <div id="tableTopLine">
-        <p>Total number of products: {productsData.length}</p>
-        <Button text="Add product"></Button>
-      </div>
+      {productsData.length == 0 ? (
+        <h2>There is no product to show.</h2>
+      ) : (
+        // incase no product in list, we should know about that.
+        <div>
+          <div id="tableTopLine">
+            <p>Total number of products: {productsData.length}</p>
+            <Button text="Add product"></Button>
+          </div>
 
-      <table id="products">
-        <tbody>
-          <tr>
-            {columnNamesList.map((head, index) => (
-              <th key={index}>{head}</th>
-            ))}
-          </tr>
-          {productsData.map((product, index) => (
-            // map method will render each product
-            <tr key={index}>
-              <td>{product.productId}</td>
-              <td>{product.productName}</td>
-              <td>{product.scrumMasterName}</td>
-              <td>{product.productOwnerName}</td>
-              <td>
-                <ol>
-                  {product.developers.map((developer, index) => (
-                    // map method will render each developer's name
-                    <li key={index}>{developer}</li>
-                  ))}
-                </ol>
-              </td>
-              <td>{product.startDate}</td>
-              <td>{product.methodology}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+          <table id="products">
+            <tbody>
+              <tr>
+                {columnNamesList.map((head, index) => (
+                  <th key={index}>{head}</th>
+                ))}
+              </tr>
+              {productsData.map((product, index) => (
+                // map method will render each product
+                <tr key={index}>
+                  <td>{product.productId}</td>
+                  <td>{product.productName}</td>
+                  <td>{product.scrumMasterName}</td>
+                  <td>{product.productOwnerName}</td>
+                  <td>
+                    <ol>
+                      {product.developers.map((developer, index) => (
+                        // map method will render each developer's name
+                        <li key={index}>{developer}</li>
+                      ))}
+                    </ol>
+                  </td>
+                  <td>{product.startDate}</td>
+                  <td>{product.methodology}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
