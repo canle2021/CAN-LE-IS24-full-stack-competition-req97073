fcc213e5b5769fd14fd818eb69d8a9b04113061b
Can Le <canle360@gmail.com>
2023-03-30 22:29:38 -0600

    add margin for whole app

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./pages/header";
 import HomePage from "./pages/homePage";
+import "./App.css";
 function App() {
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
